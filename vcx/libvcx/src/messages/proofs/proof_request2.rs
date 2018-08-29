c03f68276ae8e4d0c985853775166c29d3b6245b
Ryan Marsh <ryan.marsh44@gmail.com>
2018-08-29 15:40:07 -0600

    started versioning

--- vcx/libvcx/src/messages/proofs/proof_request2.rs
+++ vcx/libvcx/src/messages/proofs/proof_request2.rs
@@ -3,6 +3,7 @@ extern crate serde_json;
 
 use std::collections::HashMap;
 use std::vec::Vec;
+use std::fmt;
 use utils::error;
 use messages::validation;
 
@@ -13,7 +14,7 @@ static REQUESTED_PREDICATES: &str = "requested_predicates";
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 struct ProofType {
-//    name: String,
+    name: String,
     #[serde(rename = "version")]
     type_version: String,
 }
@@ -57,26 +58,32 @@ pub struct ProofPredicates {
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct ProofRequestData{
-//    nonce: String,
-//    name: String,
-//    #[serde(rename = "version")]
-//    data_version: String,
+    nonce: String,
+    name: String,
+    #[serde(rename = "version")]
+    data_version: String,
     pub requested_attributes: HashMap<String, AttrInfo>,
-//    pub requested_predicates: HashMap<String, PredicateInfo>,
+    pub requested_predicates: HashMap<String, PredicateInfo>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct ProofRequestMessage{
     #[serde(rename = "@type")]
     type_header: ProofType,
-//    #[serde(rename = "@topic")]
-//    topic: ProofTopic,
+    #[serde(rename = "@topic")]
+    topic: ProofTopic,
     pub proof_request_data: ProofRequestData,
-//    pub msg_ref_id: Option<String>,
+    pub msg_ref_id: Option<String>,
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
-pub struct ProofRequestBuilder {
+pub struct ProofRequestBuilder {}
+impl ProofRequestBuilder {
+    fn v1() -> ProofRequestBuilder_v1 {
+
+    }
+}
+pub struct ProofRequestBuilder_v1 {
     tid: Option<Result<u32, u32>>,
     mid: Option<Result<u32, u32>>,
     type_version: Option<Result<String, u32>>,
@@ -84,7 +91,7 @@ pub struct ProofRequestBuilder {
     name: Option<Result<String, u32>>,
     data_version: Option<Result<String, u32>>,
     requested_attributes: Option<Result<HashMap<String, AttrInfo>, u32>>,
-    requested_predicates: Option<Result<String, u32>>,
+    requested_predicates: Option<Result<HashMap<String, PredicateInfo>, u32>>,
     msg_ref_id: Option<Result<String, u32>>
 }
 
@@ -92,6 +99,10 @@ pub trait MsgUtils {
     fn mandatory_field<T>(&self, field: Option<Result<T, u32>>) -> Result<T, u32> {
         field.ok_or(error::MISSING_MSG_FIELD.code_num)?
     }
+
+    fn wrap_ok<T>(&self, field: T) -> Option<Result<T, u32>>  { Some(Ok(field)) }
+
+    fn wrap_err(&self, err: u32) -> Option<Result<T, u32>> { Some(Err(err))}
 }
 impl MsgUtils for ProofRequestBuilder {}
 impl ProofRequestBuilder {
@@ -110,7 +121,32 @@ impl ProofRequestBuilder {
     }
 
     pub fn type_version(mut self, version: &str) -> ProofRequestBuilder {
-        self.type_version = Some(Ok(version.to_string()));
+        self.type_version = self.wrap_ok(version.to_string());
+        self
+    }
+
+    pub fn tid(mut self, tid: u32) -> ProofRequestBuilder {
+        self.tid = self.wrap_ok(tid);
+        self
+    }
+
+    pub fn mid(mut self, mid: u32) -> ProofRequestBuilder {
+        self.mid = self.wrap_ok(mid);
+        self
+    }
+
+    pub fn nonce(mut self, nonce: &str) -> ProofRequestBuilder {
+        self.nonce = Some(validation::validate_nonce(nonce));
+        self
+    }
+
+    pub fn proof_name(mut self, name: &str) -> ProofRequestBuilder {
+        self.name = self.wrap_ok(name.to_string());
+        self
+    }
+
+    pub fn proof_data_version(mut self, version: &str) -> ProofRequestBuilder {
+        self.data_version = self.wrap_ok(version.to_string());
         self
     }
 
@@ -120,7 +156,7 @@ impl ProofRequestBuilder {
             Ok(a) => a,
             Err(e) => {
                 debug!("Cannot parse attributes: {}", e);
-                self.requested_attributes = Some(Err(error::INVALID_JSON.code_num));
+                self.requested_attributes = self.wrap_err(error::INVALID_JSON.code_num);
                 return self
             }
         };
@@ -132,22 +168,73 @@ impl ProofRequestBuilder {
                 check_req_attrs.insert(attr.name.clone(), attr.clone());
             }
         }
-        self.requested_attributes = Some(Ok(check_req_attrs));
+        self.requested_attributes = self.wrap_ok(check_req_attrs);
+        self
+    }
+
+    pub fn requested_predicates(mut self, predicates: &str) -> ProofRequestBuilder {
+        let mut check_predicates: HashMap<String, PredicateInfo> = HashMap::new();
+        let attr_values: Vec<PredicateInfo> = match serde_json::from_str(predicates) {
+            Ok(a) => a,
+            Err(e) => {
+                debug!("Cannot parse predicates: {}", e);
+                self.requested_predicates = self.wrap_err(error::INVALID_JSON.code_num);
+                return self
+            },
+        };
+
+        for (index, attr) in attr_values.iter().enumerate() {
+            if check_predicates.contains_key(&attr.name) {
+                check_predicates.insert(format!("{}_{}", attr.name, index), attr.clone());
+            } else {
+                check_predicates.insert(attr.name.clone(), attr.clone());
+            }
+        }
+
+        self.requested_predicates = self.wrap_ok(check_predicates);
         self
     }
 
     pub fn build(self) -> Result<ProofRequestMessage, u32> {
         Ok(ProofRequestMessage {
             type_header: ProofType {
+                name: String::from(PROOF_REQUEST),
                 type_version: self.mandatory_field(self.type_version.clone())?
             },
+            topic: ProofTopic {
+                tid: self.mandatory_field(self.tid.clone())?,
+                mid: self.mandatory_field(self.mid.clone())?,
+            },
             proof_request_data: ProofRequestData {
-                requested_attributes: self.mandatory_field(self.requested_attributes.clone())?
-            }
+                nonce: self.mandatory_field(self.requested_attributes.clone())?,
+                name: self.mandatory_field(self.name.clone())?,
+                data_version: self.mandatory_field(self.data_version.clone())?,
+                requested_attributes: self.mandatory_field(self.requested_attributes.clone())?,
+                requested_predicates: self.mandatory_field(self.requested_predicates.clone())?
 
+            }
         })
     }
 }
+
+impl fmt::Display for ProofRequestMessage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match serde_json::to_string(&self) {
+            Ok(x) => {
+                write!(f, "{}", s)
+            },
+            Err(e) => {
+                error!("{}: {:?}", error::INVALID_PROOF_REQ.message, e);
+                write!(f, "null")
+            }
+        }
+    }
+}
+
+impl ProofRequestMessage {
+    pub fn get_proof_request_data(&self) -> String { json!(self)[PROOF_DATA].to_string() }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -173,5 +260,37 @@ mod tests {
             .build();
 
         assert_eq!(rc, Err(error::INVALID_JSON.code_num));
+    }
+
+    #[test]
+    fn test_proof_request_msg() {
+        //proof data
+        let data_name = "Test";
+        let nonce = "123432421212";
+        let data_version = "3.75";
+        let attrs = "";
+        let version = "1.3";
+        let tid = 89;
+        let mid = 98;
+
+        let mut request = ProofRequestBuilder::new()
+            .type_version(version)
+            .tid(tid)
+            .mid(mid)
+            .nonce(nonce)
+            .proof_name(data_name)
+            .proof_data_version(data_version)
+            .requested_attrs(REQUESTED_ATTRS)
+            .requested_predicates(REQUESTED_PREDICATES)
+            .build()
+            .unwrap();
+
+        let serialized_msg = request.to_string().unwrap();
+        println!("{}", serialized_msg);
+        assert!(serialized_msg.contains(r#""@type":{"name":"PROOF_REQUEST","version":"1.3"}"#));
+        assert!(serialized_msg.contains(r#"@topic":{"mid":98,"tid":89}"#));
+        assert!(serialized_msg.contains(r#"proof_request_data":{"nonce":"123432421212","name":"Test","version":"3.75","requested_attributes""#));
+
+        assert!(serialized_msg.contains(r#""age":{"name":"age","restrictions":[{"schema_id":"6XFh8yBzrpJQmNyZzgoTqB:2:schema_name:0.0.11","schema_issuer_did":"6XFh8yBzrpJQmNyZzgoTqB","schema_name":"Faber Student Info","schema_version":"1.0","issuer_did":"8XFh8yBzrpJQmNyZzgoTqB","cred_def_id":"8XFh8yBzrpJQmNyZzgoTqB:3:CL:1766"},{"schema_id":"5XFh8yBzrpJQmNyZzgoTqB:2:schema_name:0.0.11","schema_issuer_did":"5XFh8yBzrpJQmNyZzgoTqB","schema_name":"BYU Student Info","schema_version":"1.0","issuer_did":"66Fh8yBzrpJQmNyZzgoTqB","cred_def_id":"66Fh8yBzrpJQmNyZzgoTqB:3:CL:1766"}]}"#));
     }
 }
--- vcx/libvcx/src/utils/error.rs
+++ vcx/libvcx/src/utils/error.rs
@@ -98,6 +98,7 @@ pub static DID_ALREADY_EXISTS_IN_WALLET: Error = Error { code_num: 1083, message
 pub static DUPLICATE_MASTER_SECRET: Error = Error { code_num: 1084, message: "Attempted to add a Master Secret that already existed in wallet"};
 pub static THREAD_ERROR: Error = Error{ code_num: 1085, message: "Unable to create thread"};
 pub static MISSING_MSG_FIELD: Error = Error{ code_num: 1086, message: "Mandatory message field missing"};
+pub static INVALID_PROOF_REQ: Error = Error{ code_num: 1087, message: "Proof Request is invalid"};
 
 lazy_static! {
     static ref ERROR_C_MESSAGES: HashMap<u32, CString> = {
@@ -189,6 +190,7 @@ lazy_static! {
         insert_c_message(&mut m, &INVALID_LEDGER_RESPONSE);
         insert_c_message(&mut m, &THREAD_ERROR);
         insert_c_message(&mut m, &MISSING_MSG_FIELD);
+        insert_c_message(&mut m, &INVALID_PROOF_REQ);
         m
     };
 }
